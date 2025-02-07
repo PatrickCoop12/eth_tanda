239ce146386950c46c2288a12f15afb01618a9ff
PatrickCoop12 <p.coopr09@gmail.com>
2025-02-06 20:09:42 -0800

    Changed architecture of agent to allow for session chat_history messages to be passed as additional input from frontend

--- Backend/app/api/api.py
+++ Backend/app/api/api.py
@@ -1,5 +1,6 @@
 from langchain_openai import ChatOpenAI
-from langgraph.prebuilt import create_react_agent
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.agents import create_tool_calling_agent, AgentExecutor
 from langchain_core.messages import HumanMessage, SystemMessage
 from cdp_langchain.agent_toolkits import CdpToolkit
 from cdp_langchain.utils import CdpAgentkitWrapper
@@ -16,10 +17,21 @@ toolkit = CdpToolkit.from_cdp_agentkit_wrapper(cdp)
 
 tools = toolkit.get_tools()
 llm = ChatOpenAI(model='gpt-4o-mini')
-react_agent = create_react_agent(llm, tools)
 
-def generate_response(inquiry:str):
-    response = react_agent.invoke({"messages":[HumanMessage(content=inquiry)]})
+from langchain_core.prompts import ChatPromptTemplate
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are a customer service agent assigned to assist clients by collecting pertinent information about how their group would like to structure their Etherium Tanda association, and answer any questions they may have about tandas or smart contracts. Use the conversation history below to respond to questions conversationally.\n {chat_history}"),
+    ('user', "{input}"),
+    ("placeholder", "{agent_scratchpad}")])
+
+agent = create_tool_calling_agent(llm, tools=tools, prompt=prompt2)
+
+tanda_formation_agent = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+
+def generate_response(inquiry:str, chat_history:str):
+    response = tanda_formation_agent.invoke({"input":[HumanMessage(content=inquiry),
+                                             "chat_history": chat_history]})
 
     return response
 
