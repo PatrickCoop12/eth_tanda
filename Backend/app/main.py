5125c87493085948c0f928721e5cc564d454d00c
PatrickCoop12 <p.coopr09@gmail.com>
2025-02-08 15:37:11 -0800

    combined agent tools to address consistency issue processing contract templates and moved into app files from notebook sandbox

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -3,13 +3,16 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from api import api
+from cdp_langchain.agent_toolkits import CdpToolkit
+from cdp_langchain.utils import CdpAgentkitWrapper
 
 
 class Query(BaseModel):
     inquiry: str
     chat_history: str
 
-
+cdp= CdpAgentkitWrapper()
+toolkit = CdpToolkit.from_cdp_agentkit_wrapper(cdp)
 # Create app and adding middleware (as good practice)
 app = FastAPI()
 
@@ -26,6 +29,7 @@ app.add_middleware(
 
 @app.post("/chat_response")
 def chat_with_agent(query:Query):
+    toolkit.get_tools()[13].invoke(input={"asset_id": "eth"})
     return api.generate_response(query.inquiry, query.chat_history)
 
 @app.get("/chat")
